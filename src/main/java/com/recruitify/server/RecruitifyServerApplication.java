b280a0d3d29ec7c8ac30e935dcdba1c2cf693d33
HungPig <hungthanhhung37@gmail.com>
2025-08-08 15:52:05 +0700

    feat(server): Create Java Spring Boot project

---
+++ src/main/java/com/recruitify/server/RecruitifyServerApplication.java
@@ -0,0 +1,13 @@
+package com.recruitify.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitifyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitifyServerApplication.class, args);
+	}
+
+}
