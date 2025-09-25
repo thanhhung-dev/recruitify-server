593e5c0ab7efe7722eb3b8638f4aa5dd4859d731
HungPig <hungthanhhung37@gmail.com>
2025-09-25 16:10:27 +0700

    feat(server): implement GlobalExceptionHandler for consistent error responses

---
+++ src/main/java/com/recruitify/server/Util/Error/GlobalException.java
@@ -0,0 +1,32 @@
+package com.recruitify.server.Util.Error;
+
+import com.recruitify.server.entities.Res.RestResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<RestResponse<Object>> handleAllException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        res.setMessage(ex.getMessage());
+        res.setError("Internal Server Error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+    }
+
+
+    @ExceptionHandler(value = {
+            IdInvalidException.class,
+    })
+    public ResponseEntity<RestResponse<Object>> handleIdException(Exception ex) {
+        RestResponse<Object> res = new RestResponse<Object>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setMessage(ex.getMessage());
+        res.setError("Exception occurs...");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+
+}
